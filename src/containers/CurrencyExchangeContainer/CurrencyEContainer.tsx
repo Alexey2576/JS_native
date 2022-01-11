a9deb5bd4c3ac3d2191c6cae4ea7a6c72c9809b3
Alexey2576 <alexeynikinitin@yandex.by>
2022-01-12 00:15:12 +0300

    Use hook useDispatch

--- src/containers/CurrencyExchangeContainer/CurrencyEContainer.tsx
+++ src/containers/CurrencyExchangeContainer/CurrencyEContainer.tsx
@@ -8,7 +8,7 @@ import {
    ChangeCurrentCurrencyAC,
    CurrencyReducersTypes
 } from '../../redux/actions';
-import {connect, ConnectedProps} from 'react-redux';
+import {connect, ConnectedProps, useDispatch} from 'react-redux';
 
 const CurrencyEContainer: React.FC<TProps> = props => {
 
@@ -21,10 +21,12 @@ const CurrencyEContainer: React.FC<TProps> = props => {
       // setCurrencyAmount,
       // setAction,
       // changeCurrency,
-      ChangeCurrentCurrencyAC,
-      ChangeCurrencyFieldAC,
-      ChangeActionAC,
+      // ChangeCurrentCurrencyAC,
+      // ChangeCurrencyFieldAC,
+      // ChangeActionAC,
    } = props;
+
+   const dispatch = useDispatch<Dispatch<CurrencyReducersTypes>>()
 
    let currencyRate: number = 0;
    const currenciesName = currencies.map((currency: CurrencyType) => {
@@ -42,30 +44,42 @@ const CurrencyEContainer: React.FC<TProps> = props => {
          if (trigger === 'byn') {
             if (value === '') {
                // setCurrencyAmount(value, value);
-               ChangeCurrencyFieldAC(value, value);
+               // ChangeCurrencyFieldAC(value, value);
+               dispatch(ChangeCurrencyFieldAC(value, value));
             } else {
                // setCurrencyAmount(value, (+Number(value).toFixed(2) / currencyRate).toFixed(2));
-               ChangeCurrencyFieldAC(value, (+Number(value).toFixed(2) / currencyRate).toFixed(2));
+               // ChangeCurrencyFieldAC(value, (+Number(value).toFixed(2) / currencyRate).toFixed(2));
+               dispatch(ChangeCurrencyFieldAC(value, (+Number(value).toFixed(2) / currencyRate).toFixed(2)));
             }
          } else {
             if (value === '') {
                // setCurrencyAmount(value, value);
-               ChangeCurrencyFieldAC(value, value);
+               // ChangeCurrencyFieldAC(value, value);
+               dispatch(ChangeCurrencyFieldAC(value, value));
             } else {
                // setCurrencyAmount((+Number(value).toFixed(2) * currencyRate).toFixed(2), value);
-               ChangeCurrencyFieldAC((+Number(value).toFixed(2) * currencyRate).toFixed(2), value);
+               // ChangeCurrencyFieldAC((+Number(value).toFixed(2) * currencyRate).toFixed(2), value);
+               dispatch(ChangeCurrencyFieldAC((+Number(value).toFixed(2) * currencyRate).toFixed(2), value));
             }
          }
       }
    };
    const changeAction = (e: React.MouseEvent<HTMLSpanElement>) => {
-      // e.currentTarget.dataset.action === 'buy' ? setAction(true) : setAction(false);
-      e.currentTarget.dataset.action === 'buy' ? ChangeActionAC(true) : ChangeActionAC(false);
+      // e.currentTarget.dataset.action === 'buy'
+         // ? setAction(true)
+         // : setAction(false);
+      // e.currentTarget.dataset.action === 'buy'
+         // ? ChangeActionAC(true)
+         // : ChangeActionAC(false);
+      e.currentTarget.dataset.action === 'buy'
+         ? dispatch(ChangeActionAC(true))
+         : dispatch(ChangeActionAC(false));
    };
 
    const changeCurrentCurrency = (e: React.MouseEvent<HTMLLIElement>) => {
       // e.currentTarget.dataset.currency && changeCurrency(e.currentTarget.dataset.currency);
-      e.currentTarget.dataset.currency && ChangeCurrentCurrencyAC(e.currentTarget.dataset.currency);
+      // e.currentTarget.dataset.currency && ChangeCurrentCurrencyAC(e.currentTarget.dataset.currency);
+      e.currentTarget.dataset.currency && dispatch(ChangeCurrentCurrencyAC(e.currentTarget.dataset.currency));
    };
 
    return (
@@ -114,13 +128,13 @@ const mapStateToProps = ({currency}: { currency: CurrencyState }): CurrencyState
 //       },
 //    };
 // };
-const actionCreators = {
-   ChangeCurrencyFieldAC,
-   ChangeActionAC,
-   ChangeCurrentCurrencyAC,
-}
+// const actionCreators = {
+//    ChangeCurrencyFieldAC,
+//    ChangeActionAC,
+//    ChangeCurrentCurrencyAC,
+// }
 
-const connector = connect(mapStateToProps, actionCreators);
+const connector = connect(mapStateToProps, {});
 
 type TProps = ConnectedProps<typeof connector>;
 
